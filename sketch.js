048e41348790c1202171bb09b4eb5ddafc328f51
Stanley Zuo <sjrzuo@gmail.com>
2017-04-19 13:24:57 -0400

    Update sketch.js
    Added Text align

--- sketch.js
+++ sketch.js
@@ -12,19 +12,23 @@ function draw() {
   background(86,6,140);
   fill(255);
   textSize(20);
-  text("Mouse click to mark activity as complete", width/1.5, 50);
-  text("Press any key to skip item", width/1.5, 100);
-  text("Things to do: "+(toDo.length + skipped.length),  10, 50);
-  text("Things completed: " +complete.length, 10, 100);
+  textAlign(RIGHT);
+  text("Mouse click to mark activity as complete", width-20, 50);
+  text("Press any key to skip item", width-20, 100);
+  textAlign(LEFT);
+  text("Things to do: "+(toDo.length + skipped.length), 20, 50);
+  text("Things completed: " +complete.length, 20, 100);
   
   fill(255,0,0);
+  textAlign(CENTER);
   // for(var i = 0; i<toDo.length; i++){
-    text("" + toDo[0], width/4, 100);
+    text("" + toDo[0], width/2, 100);
   // }
   
   fill(0,255,0);
+  textAlign(LEFT);
   for(var x = 0; x<complete.length; x++){
-    text("Complete: " + complete[x], 10, (x*20)+150);
+    text("Complete: " + complete[x], 20, (x*20)+150);
   }
 }
 
