a466e55312ec72d778d8f57fef4e0734b037a6fd
Stanley Zuo <sjrzuo@gmail.com>
2017-04-19 23:25:03 -0400

    Update sketch.js
    Display skipped items

--- sketch.js
+++ sketch.js
@@ -11,6 +11,7 @@ function setup() {
 function draw() {
   background(86,6,140);
   fill(255);
+  noStroke();
   textSize(20);
   textAlign(RIGHT);
   text("Mouse click to mark activity as complete", width-20, 50);
@@ -19,17 +20,27 @@ function draw() {
   text("Things to do: "+(toDo.length + skipped.length), 20, 50);
   text("Things completed: " +complete.length, 20, 100);
   
-  fill(255,0,0);
-  textAlign(CENTER);
-  // for(var i = 0; i<toDo.length; i++){
-    text("" + toDo[0], width/2, 100);
-  // }
-  
+  textSize(10);
   fill(0,255,0);
   textAlign(LEFT);
   for(var x = 0; x<complete.length; x++){
     text("Complete: " + complete[x], 20, (x*20)+150);
   }
+  
+  fill(255,0,0);
+  textAlign(RIGHT);
+  for(var y = 0; y<skipped.length; y++){
+    text("Skipped: " + skipped[y], width-20, (y*20)+150);
+  }
+  
+  textSize(40);
+  textAlign(CENTER);
+  fill(255);
+  stroke(0);
+  strokeWeight(5);
+  // for(var i = 0; i<toDo.length; i++){
+    text("" + toDo[0], width/2, height/2);
+  // }
 }
 
 function mousePressed() {
